e91655d8f824a9a0a10c83fb706f8ec54e6f8ed6
MOKAITECH\luchuangchuang <1145230944@qq.com>
2018-01-22 16:03:42 +0800

    EEE

--- Main.py
+++ Main.py
@@ -1,99 +1,98 @@
-# this module use amap api to get all the factories of beichen discrit of tianjin
-# thanks to amap company. please visit http://ditu.amap.com/ to get more information
-
-# coding:utf-8
-
-from urllib import request
-import urllib
-import  requests
-
-import xml.dom.minidom as mdom
-import xml
-import string
+import requests
+#导入sqlite3库文件
+import sqlite3
+import json
+from bs4 import BeautifulSoup
 
 
-file_name = 'result.txt'  # write result to this file
-url_amap = r'http://restapi.amap.com/v3/place/text?key=eede91fe8ab216f9b5a1d675f1b00f53&keywords=北京大学&types=高等院校&city=北京&children=1&offset=20&page=1&extensions=all'
-facility_type = r'types=高等院校'  # factory facilities
-region = r'city=北京'  # beichen of tianjin
-each_page_rec = 20  # results that displays in one page
-which_pach = r'page=1'  # display which page
-xml_file = 'tmp.xml'  # xml filen name
+id=''
+name=''
+tag=''
+types =''
+location =''
+address =''
+#数据库存在时自己链接，如果不存在就新建一个相应的db文件
+conn=sqlite3.connect("./Test.db")
+#创建表
+conn.execute('''CREATE TABLE DataTable
+     (Namee  TEXT   NULL,
+       ID   TEXT    NULL ,
+       Tag            TEXT   NULL,
+        Types          TEXT   NULL,
+        Location      TEXT   NULL,
+        Address       TEXT   NULL);''')
+print("SuccessOpenDatabase")
+# 向表中插入记录
+# 注意sql语句中使用了格式化输出的占位符%s和%d来表示将要插入的变量，其中%s需要加引号'
+
+#署名：cpf
+headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+           'Accept-Encoding': 'gzip, deflate, compress',
+           'Accept-Language': 'en-us;q=0.5,en;q=0.3',
+           'Cache-Control': 'max-age=0',
+           'Connection': 'keep-alive',
+           'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:22.0) Gecko/20100101 Firefox/22.0'}
+
+keywords = "美食"
+city= "zhengzhou"
+page = 1
+key = "eede91fe8ab216f9b5a1d675f1b00f53"
+
+url = "http://restapi.amap.com/v3/place/text?&keywords={keywords}" \
+      "&city={city}&output=json&offset=20&page={page}&key={key}&extensions=all"
 
 
-# write logs
-def log2file(file_handle, text_info):
-    file_handle.write(text_info)
-
-
-# get html by url and save the data to xml file
-def getHtml(url):
-    page = requests.get(url)
-    html = page.text
-    #html=html.decode()
-
+def gethtml(url,keywords,city,page,key,headers):
+    url = url.format(keywords = keywords ,city = city,page = page ,key = key)
     try:
-        # open xml file and save data to it
-        with open(xml_file, 'w') as xml_file_handle:
-            xml_file_handle.write(html)
-    except IOError as err:
-        print("ffff")
-
-        return -1
-
-    return 0
-
-
-# phrase data from xml
-def parseXML():
-    total_rec = 1  # record number
-
-    # open xml file and get data record
-    try:
-        with open(file_name, 'r') as file_handle:
-            dom = mdom.parse(xml_file)
-            root = dom.getElementsByTagName(
-                "response")  # The function getElementsByTagName returns NodeList.
-
-            for node in root:
-                total_rec = node.getElementsByTagName('count')[0].childNodes[0].nodeValue
-
-                pois = node.getElementsByTagName("pois")
-                for poi in pois[0].getElementsByTagName('poi'):
-                    name = poi.getElementsByTagName("name")[0].childNodes[0].nodeValue
-                    location = poi.getElementsByTagName("location")[0].childNodes[
-                        0].nodeValue
-                    text_info = '' + name + ',' + location + '\n'
-
-                    # save data record
-                    log2file(file_handle, text_info.encode('utf-8'))
-
-    except IOError as err:
-        print("IO error: " + str(err))
-
-    return total_rec
-
-
-if __name__ == '__main__':
-    if getHtml(url_amap) == 0:
-        # parse the xml file and get the total record number
-        total_record_str = parseXML()
-
-        total_record = string.atoi(str(total_record_str))
-        if (total_record % each_page_rec) != 0:
-            page_number = total_record / each_page_rec + 2
-        else:
-            page_number = total_record / each_page_rec + 1
-
-            # retrive the other records
-        for each_page in range(2, page_number):
-
-            url_amap = url_amap.replace('page=' + str(each_page - 1),
-                                        'page=' + str(each_page))
-            getHtml(url_amap)
-            parseXML()
-
+        r = requests.get(url,headers = headers)
+        r.raise_for_status()
+    except requests.RequestException as e:
+        print(e)
     else:
-        print('error: fail to get xml from amap')
+        result = r.json()
+        return result
 
 
+def parse(jsons,details,id,name,tag,types,location,address):
+    try:
+        for item in jsons['pois']:
+            id = item['id']
+
+            name = item['name']
+
+            tag = item['tag']
+
+            types = item['type']
+
+            location = item['location']
+            address =item['address']
+
+            restaurant = {'id' :id, 'name' :name ,'tag' : tag,
+                        'type' : types,'location' :location,'address' : address}
+            details.append(restaurant)
+            sql = "INSERT INTO DataTable(Namee,ID ,Tag,Types,Location,Address)VALUES ('%s','%s','%s','%s','%s','%s')" % (name, id, tag, types, location, address)
+            conn.execute(sql)
+            print(id)
+
+    except:
+        pass
+
+def loop(num):
+    global page
+    while page < num:
+        jsons = gethtml(url, keywords, city, page, key, headers)
+        parse(jsons, details,id,name,tag,types,location,address)
+        page = page + 1
+
+details = []
+loop(20)
+for i in details:
+
+
+    print(id)
+
+
+    #print(i)
+conn.commit()
+conn.close()
